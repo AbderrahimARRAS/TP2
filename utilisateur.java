8abaea3f2be5d87b9039d9c6f99bdfcac85b48a8
AbderrahimARRAS <124909735+AbderrahimARRAS@users.noreply.github.com>
2023-03-08 00:10:40 +0100

    Add files via upload

---
+++ utilisateur.java
@@ -0,0 +1,30 @@
+package TP2_2;
+
+public class utilisateur implements java.io.Serializable{
+
+  
+	
+
+	public String id;
+	public String pass;
+	public utilisateur(String nom,String pass) {
+		this.id=nom;
+		this.pass=pass;
+	     
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+}
+
